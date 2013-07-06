afce63d4003cb30a79303c09ce10251762683ba6
Shirish <shirishreddy89@gmail.com>
2013-07-06 08:27:06 +0000

    Adding use string

--- web.js
+++ web.js
@@ -4,19 +4,22 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var res = readFile('index.html');
-  response.send(res);
+    "use strict";
+    var res = readFile('index.html');
+    response.send(res);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    "use strict";
+    console.log("Listening on " + port);
 });
 
 var readFile = function(filename) {
-  var buf = fs.readFileSync(filename);
-  var res = buf.toString();
-  return res;
+    "use strict";
+    var buf = fs.readFileSync(filename);
+    var res = buf.toString();
+    return res;
 };
 
 //console.log(readFile('index.html'));
