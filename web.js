d0094421c8e5afb38914ef129b6cf8c5a172583d
Shirish <shirishreddy89@gmail.com>
2013-07-06 07:35:32 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function(request, respons  e) {
+  var buf = fs.readFile('index.html');
+  var res = buf.toString();
+  response.send(res);
 });
 
 var port = process.env.PORT || 5000;
